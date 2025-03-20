c44fd468f33ae095bf3d8ae7cbf5ce806d474253
16so03 <laumovas@gmail.com>
2025-03-19 20:40:02 -0700

    Initial commit

---
+++ src/test/java/com/survivors/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.survivors.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+        "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        "json:target/cucumber.json"},
+        features = "src/test/resources/features",
+        glue ="com/survivors/step_defs",
+        dryRun = false,
+        tags = "",
+        publish = true
+
+)
+
+public class CukesRunner {
+
+
+}
